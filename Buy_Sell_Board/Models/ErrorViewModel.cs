acf9a371e258663deb5193a6f207fe8286846ebd
Dice_kl17 <Dice_kl17@student.itstep.org>
2021-11-11 00:40:17 +0200

    Добавьте файлы проекта.

---
+++ Buy_Sell_Board/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Buy_Sell_Board.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
